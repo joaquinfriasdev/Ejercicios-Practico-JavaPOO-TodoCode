28ebc4337b5933be0e811c3e65ae3b3bb1384362
Joaquín Frías <119880493+joaquinfriasdev@users.noreply.github.com>
2023-09-25 21:10:18 -0300

    Modelado automovil

--- automovil/src/main/java/concesionaria/automovil/Automovil.java
+++ automovil/src/main/java/concesionaria/automovil/Automovil.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package concesionaria.automovil;
 
-/**
- *
- * @author Joaquín Frías 
- */
+
 public class Automovil {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      
+        
+        
     }
 }
