0a9714c137b80cb58f7c475f17c53543f2b3b3cb
Joaquín Frías <119880493+joaquinfriasdev@users.noreply.github.com>
2023-10-01 20:01:14 -0300

    Adaptando el proyecto para el  sistema de roles y permisos

--- Login/src/main/java/com/mycompany/login/Login.java
+++ Login/src/main/java/com/mycompany/login/Login.java
@@ -8,7 +8,7 @@ import com.mycompany.login.igu.Principal;
 public class Login {
 
     public static void main(String[] args) {
-        
+        // Trabajaremos sobre este prouyecta agregando un sistema de roles y permisos
         Principal princ = new Principal();
         princ.setVisible(true);
         princ.setLocationRelativeTo(null);
