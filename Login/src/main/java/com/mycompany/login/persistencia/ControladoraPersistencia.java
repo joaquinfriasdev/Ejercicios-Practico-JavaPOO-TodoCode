aa37d660d2bfcc0e0a9db76b7917248aae652d7e
Joaquín Frías <119880493+joaquinfriasdev@users.noreply.github.com>
2023-10-03 16:36:24 -0300

    Creo la clase Rol y hago el remapeo

--- Login/src/main/java/com/mycompany/login/persistencia/ControladoraPersistencia.java
+++ Login/src/main/java/com/mycompany/login/persistencia/ControladoraPersistencia.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class ControladoraPersistencia {
     
     UsuarioJpaController usuJpa = new UsuarioJpaController();
-
+    RolJpaController rolJpa = new RolJpaController();
+    
     public List<Usuario> traerUsuarios() {
         List<Usuario> listaUsuario = usuJpa.findUsuarioEntities();
         return listaUsuario;
